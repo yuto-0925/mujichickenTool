cfb6f3c3c8e620d5b077c96ec59b9dddd46aabbc
yuto-0925 <yutoyuto0925@gmail.com>
2021-06-28 08:40:35 +0900

    4 commit

--- mujichicken.py
+++ mujichicken.py
@@ -17,8 +17,12 @@ def now_time():
 
 def mujichicken_insta(username, password, tagName, likedMax):
 
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+
 #ブラウザに接続
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
 
 #インスタのURLにアクセス
     driver.get("https://www.instagram.com/accounts/login/")
