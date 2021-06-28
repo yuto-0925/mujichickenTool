c3f6368c6c9e55946d0a22f699ad6d3cebb86fd5
yuto-0925 <yutoyuto0925@gmail.com>
2021-06-28 13:49:08 +0900

    10 commit

--- mujichicken.py
+++ mujichicken.py
@@ -37,13 +37,12 @@ def mujichicken_insta(username, password, tagName, likedMax):
 
 #インスタのURLにアクセス
     driver.get("https://www.instagram.com/accounts/login/")
-    driver.implicitly_wait(10)
     time.sleep(1)
 
 #メアドと、パスワードを入力
-    driver.find_element_by_name('sp_sb0').send_keys(username)
+    driver.find_element_by_name('username').send_keys(username)
     time.sleep(2)
-    driver.find_element_by_name('Y161634y').send_keys(password)
+    driver.find_element_by_name('password').send_keys(password)
     time.sleep(2)
 
 #ログインボタンを押す
