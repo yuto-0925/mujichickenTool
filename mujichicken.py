9b6c05f376d12a2e846f7eebb689c47960f7a304
yuto-0925 <yutoyuto0925@gmail.com>
2021-06-28 19:13:45 +0900

    10 commit

--- mujichicken.py
+++ mujichicken.py
@@ -22,6 +22,7 @@ def mujichicken_insta(username, password, tagName, likedMax):
 
     options = Options()
     options.add_argument('--disable-extensions')
+    options.add_argument("--start-maximized")
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
