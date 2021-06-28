cfcc1db38d9667a725b1f65ae9fbf979747e3544
yuto-0925 <yutoyuto0925@gmail.com>
2021-06-28 16:22:21 +0900

    10 commit

--- mujichicken.py
+++ mujichicken.py
@@ -19,7 +19,7 @@ def now_time():
 def mujichicken_insta(username, password, tagName, likedMax):
 
     options = webdriver.ChromeOptions()
-    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
 
 #ブラウザに接続
     driver = webdriver.Chrome(options=options)
