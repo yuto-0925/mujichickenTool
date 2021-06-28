5b048a665f4c4f5124aadc6a3b276a71f311f590
yuto-0925 <yutoyuto0925@gmail.com>
2021-06-28 17:47:42 +0900

    10 commit

--- mujichicken.py
+++ mujichicken.py
@@ -21,14 +21,11 @@ def mujichicken_insta(username, password, tagName, likedMax):
 
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    options.add_argument('--lang=ja-JP')
-    options.add_argument('--proxy-server="direct://"')
-    options.add_argument('--proxy-bypass-list=*')
-
+    
 #ブラウザに接続
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
-    driver.set_window_size('1200', '1000')
+    options.add_argument('--window-size=1920,1080')
 
 #インスタのURLにアクセス
     driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
