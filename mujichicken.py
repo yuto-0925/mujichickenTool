23ccf0c2c75cdeceae53564313bac4c2d0d2ec8b
yuto-0925 <yutoyuto0925@gmail.com>
2021-06-28 13:20:05 +0900

    10 commit

--- mujichicken.py
+++ mujichicken.py
@@ -33,7 +33,7 @@ def mujichicken_insta(username, password, tagName, likedMax):
     options.add_argument("--no-sandbox")
 
 #ブラウザに接続
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
 
 #インスタのURLにアクセス
     driver.get("https://www.instagram.com/accounts/login/")
